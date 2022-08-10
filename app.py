8a0bf27014711a489522168b97d02adda5521a28
PriyaKhushboo <khushbusingh352@gmail.com>
2022-08-10 18:47:57 +0530

    changes added

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app= Flask(__name__)
 
 @app.route('/',methods = ['GET','POSt'])
 def index():
-    return 'starting machine learning project'
+    return 'starting machine learning project(CI CD pipeline)'
 
 
 if __name__=='__main__':
